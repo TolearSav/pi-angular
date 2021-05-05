6aa7c3e1cd16dda85d625fc273c736035516b43a
Luana Savian <luanasavian26@gmail.com>
2021-05-05 12:35:16 -0300

    Criação Models

---
+++ armazemNatural/src/app/model/Produto.ts
@@ -0,0 +1,15 @@
+import { Categoria } from "./Categoria"
+
+export class Produto {
+    public id: number
+    public nome: string
+    public descricao: string
+    public preco: number
+    public marca: string
+    public fornecedor: string
+    public qtdEstoque: number
+    public imagem: string
+    public peso: number
+    public ativo: boolean
+    public categoria: Categoria
+}
