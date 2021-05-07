88d5e08a939bd2b51d7508ac31e934837d27342e
Luana Savian <luanasavian26@gmail.com>
2021-05-07 14:30:39 -0300

    teste de consumo

--- armazemNatural/src/app/app-routing.module.ts
+++ armazemNatural/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "entrar", pathMatch: "full" },
+  
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 
-  { path: "entrar", component: EntrarComponent },
-  { path: "cadastrar", component: CadastrarComponent }
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
+
+  {path: 'inicio', component: InicioComponent}
+
 ];
 
 @NgModule({
--- armazemNatural/src/app/model/Usuario.ts
+++ armazemNatural/src/app/model/Usuario.ts
@@ -1,7 +1,7 @@
-export class User {
+export class Usuario {
     public id: number
     public nome: string
-    public usuario: string
+    public email: string
     public senha: string
     public foto: string
     public tipo: string
